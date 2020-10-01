330c896a292f6106039cb3febcad024acacca636
Christian <christian.mahardhika@gmail.com>
2020-10-01 22:21:57 +0900

    feat: card component and grid layout

--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -3,7 +3,7 @@ import React,{useEffect, useState} from "react";
 import styled from 'styled-components';
 
 export default function Movie() {
-  const API_KEY="95e87c5acbcfa3a1b0d4df3e1a73755b"
+  const API_KEY="FILL MEEEE~!"
   const FEATURED_API="https://api.themoviedb.org/3/discover/movie?api_key="+API_KEY+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
   const SEARCH_API="https://api.themoviedb.org/3/search/company?api_key="+API_KEY+"&query="
 
