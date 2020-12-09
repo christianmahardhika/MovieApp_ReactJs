fc395d6cd403c4dd7a506804f43ce444ae403c2a
Christian <christian.mahardhika@gmail.com>
2020-12-09 20:32:44 +0700

    fix: style usecase

--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -1,24 +1,23 @@
 import MovieComponent from "./components/MovieComponent";
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import MovieUsecase from "../../store/movieservices/usecase/MovieUsecase";
-// import MovieRepository from "./repository/MovieRepository";
+// import MovieRepository from "../../store/movieservices/repository/MovieRepository"
 
 export default function Movie() {
   // const SEARCH_API="https://api.themoviedb.org/3/search/company?api_key="+API_KEY+"&query="
   // const [movies, setMovies] = useState([]);
-  //   useEffect(() => {
-  //     MovieRepository.DiscoverMovies().then(res => { console.log(res.results); setMovies(res.results)})
-  //     })
-      useEffect(() => {
-          const movies = MovieUsecase
-        })
+  // useEffect(() => {
+  //   MovieRepository.DiscoverMovies().then(res => { console.log(res.results); setMovies(res.results)})
+  //   })
+  const moviess = MovieUsecase()
+  console.log(moviess)
   return (<div>
     <h1>React Movie Apps</h1>
     <MovieContainer>
-    {movies.length > 0 && movies.map((movie) => 
-      (<MovieComponent {...movie} />)
-    )}
+      {moviess.length > 0 && moviess.map((movie) =>
+        (<MovieComponent {...movie} />)
+      )}
     </MovieContainer>
   </div>)
 }
@@ -28,4 +27,4 @@ const MovieContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
 `;
-  
+
--- store/movieservices/usecase/MovieUsecase.ts
+++ store/movieservices/usecase/MovieUsecase.ts
@@ -1,11 +1,15 @@
 // Todo
 
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import MovieRepository from "../repository/MovieRepository";
 // import { BaseResponse } from "../repository/response";
 
 export default function MovieUsecase() {
     const [movies, setMovies] = useState([]);
-    MovieRepository.DiscoverMovies().then(res => { return setMovies(res.results);})
-    return movies
+    useEffect(() => {
+        MovieRepository.DiscoverMovies().then(res => { return setMovies(res.results);})
+      })
+    return(
+        movies
+    )
 }
